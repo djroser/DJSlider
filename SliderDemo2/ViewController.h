0898aa722dcdc87d6f11eb0f9da46193e0386dbd
djroser <user@weiwei.focuschina.com>
2015-07-30 09:35:30 +0800

    Initial Commit

---
+++ SliderDemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SliderDemo2
+//
+//  Created by user on 15/7/30.
+//  Copyright (c) 2015年 dingjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SliderDemo2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SliderDemo2
+//
+//  Created by user on 15/7/30.
+//  Copyright (c) 2015年 dingjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
